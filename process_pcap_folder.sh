0811f417768656ed7c29249029de23f0c2f09587
Federico Foschini <ffoschini@certego.net>
2020-09-19 19:40:37 +0200

    Updated pcap script to run in /tmp and append files with cat

--- process_pcap_folder.sh
+++ process_pcap_folder.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+#moving to tmp directory
+mkdir -p /tmp/zeek-files
+cd /tmp/zeek-files
 for i in `ls ${1}/*.pcap`;
 do
     echo "Processing pcap ${i}"
     zeek -C -r ${i} ${2} "${3}"
+    for zeek_output in `ls *.log`;
+    # copying zeek output files so they will not be ereased when running multiple pcaps
+    do
+        cat ${zeek_output} >> /var/log/zeek/${zeek_output}
+        rm ${zeek_output}
+    done
 done
